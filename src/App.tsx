9134e46ba6b336f4667fce6e9f9670b89eea7546
Miguel Quemado <mq003322@ohio.edu>
2024-02-26 16:24:34 -0500

    Initial Commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,41 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Box, AppBar, Toolbar, IconButton, Typography, Button } from '@mui/material';
+import MenuIcon from '@mui/icons-material/Menu';
+import { getClips } from './services/apiService';
 
 function App() {
+
+  const handleOnClick = async () => {
+    try {
+      const clips = await getClips();
+      console.log('Clips:', clips);
+    } catch (error) {
+      console.error('Error fetching clips:', error);
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Box sx={{ flexGrow: 1 }}>
+            <AppBar position="static">
+                <Toolbar>
+                    <IconButton
+                        size="large"
+                        edge="start"
+                        color="inherit"
+                        aria-label="menu"
+                        sx={{ mr: 2 }}
+                    >
+                        <MenuIcon />
+                    </IconButton>
+                    <Button variant='text' color='inherit' onClick={handleOnClick}>
+                      Test API
+                    </Button>
+                    <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+                        PhrogChair
+                    </Typography>
+                </Toolbar>
+            </AppBar>
+        </Box>
   );
 }
 
---
+++ src/services/apiService.js
@@ -0,0 +1,24 @@
+import axios from 'axios';
+
+const CORS_PROXY_URL = 'https://corsproxy.io/?';  // Route through proxy server to circumvent CORS Same-Origin Policy
+const BASE_URL = 'https://xbl.io';
+const API_KEY = 'c5b30519-47a9-450b-b2e2-23837ae64562';
+
+const apiService = axios.create({
+  baseURL: CORS_PROXY_URL + BASE_URL,
+  headers: {
+    'X-Authorization': API_KEY,
+    'Content-Type': 'application/json',
+  },
+});
+
+export const getClips = async () => {
+  try {
+    const response = await apiService.get('/api/v2/dvr/gameclips');
+    return response.data;
+  } catch (error) {
+    throw error;
+  }
+};
+
+export default apiService;
